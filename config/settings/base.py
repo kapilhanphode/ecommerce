2a873197d3907e71c3cab72b0bc02bc455d8fdcf
Kapil <kapilhanphode11@gmail.com>
2026-04-01 17:45:22 +0530

    [Updated] Code improve

--- config/settings/base.py
+++ config/settings/base.py
@@ -94,7 +94,7 @@ DATABASE_URL = config('DATABASE_URL', default=None)
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
